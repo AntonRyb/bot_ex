747c1cedba2c0111fcbf9d148b95f924a77d44d2
AntonRyb <33671099+AntonRyb@users.noreply.github.com>
2019-06-17 14:34:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let button = {
       [
       {text: 'EUR ~ €', callback_data: 'EUR'},
       {text: 'USD ~ $', callback_data: 'USD'},
-      /*{text: 'RUB ~ ₽', callback_data: 'RUB'},*/
+      
       {text: 'BTC ~ ฿', callback_data: 'BTC'}
       ]
     ]
